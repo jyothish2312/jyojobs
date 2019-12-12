136e47eed2451e9efc46e6b05c81d6c06386b1b1
jyothish2312 <42204656+jyothish2312@users.noreply.github.com>
2019-12-12 20:03:22 +0530

    Update GPACalculator.cpp

--- GPACalculator.cpp
+++ GPACalculator.cpp
@@ -4,6 +4,15 @@ int main(){
 //THE AUTHOR OF THIS PROGRAM IS || ***JYOthish K. J.*** || . ALL RIGHTS ASSOCIATED WITH THIS PROGRAM ARE RESERVED BY THE AUTHOR.
 cout<<endl;
 cout<<endl;
+
+int t;
+cout<<"PLEASE ENTER THE NUMBER OF SUBJECTS YOU HAVE ENROLLED FOR:\n";
+cin>>t;
+int grade[t];
+int cr[t];
+int q=0;
+
+
 cout<<"PLEASE REFER TO THE FOLLOWING CONVERSION CHART FOR ENTERING THE GRADE SCORED:"<<endl;
 cout<<endl;
 
@@ -29,26 +38,20 @@ i=j;
 g=g-1;
 }
 
+cout<<"= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = ="<<endl;
 
-int t;
-cout<<"PLEASE ENTER THE NUMBER OF SUBJECTS YOU HAVE ENROLLED FOR:\n";
-cin>>t;
-int grade[t];
-int cr[t];
-int q=0;
-cout<<t<<endl;
 
-cout<<"for reference of 1st year students, the credits for individual subjects are as follows: "<<endl;
-cout<<"C101 || 3 "<<endl;
-cout<<"P101 || 3 "<<endl;
-cout<<"M101 || 3 "<<endl;
-cout<<"B101 || 3 "<<endl;
-cout<<"CS141 || 2 "<<endl;
-cout<<"C141 || 2 "<<endl;
-cout<<"P141 || 2 "<<endl;
-cout<<"B141 || 2 "<<endl;
-cout<<"H101 || 2 "<<endl;
-cout<<"H109 || 2 "<<endl;
+cout<<"For reference of 1st year students, the credits for individual subjects are as follows: "<<endl;
+cout<<"C101 : 3 || ";
+cout<<"P101 : 3 ||";
+cout<<"M101 : 3 ||";
+cout<<"B101 : 3 ||";
+cout<<"CS141 : 2 ||";
+cout<<"C141 : 2 ||";
+cout<<"P141 : 2 ||";
+cout<<"B141 : 2 ||";
+cout<<"H101 : 2 ||";
+cout<<"H109 : 2 "<<endl;
 cout<<"- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"<<endl;
 cout<<"Other's can refer to *** https://drive.google.com/file/d/1b1Ns8fU0Jp_bbeUH3AGI1NYOsL___Fx0/view?usp=sharing *** for knowing the credits associated with their subjects. "<<endl;
 cout<<"- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"<<endl;
