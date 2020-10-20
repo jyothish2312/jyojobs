46a83a765a7d8fe27d2ac9cd9d0a5ab54c58012a
jyothish2312 <42204656+jyothish2312@users.noreply.github.com>
2020-10-20 12:54:04 +0530

    Update RPMCaliberationRig_RegulatedStepper.ino

--- RPMCaliberationRig_RegulatedStepper.ino
+++ RPMCaliberationRig_RegulatedStepper.ino
@@ -1,9 +1,9 @@
 #include<LiquidCrystal.h>
 float rajmachawal;
-float nirdesh;
-int deri;
+int nirdesh;
 int disha = 10;
 int kadam = 11;
+int set=13;
 
 LiquidCrystal darpan(2,3,4,5,6,7);
 
@@ -12,26 +12,51 @@ void setup() {
   darpan.begin(16,2);
   pinMode(disha,OUTPUT);
   pinMode (kadam, OUTPUT);
+  pinMode(set, INPUT);
   digitalWrite(disha, HIGH);
+  digitalWrite(kadam, LOW);
+
+ while(set){
+  nirdesh = analogRead(A0);
+  nirdesh = map(nirdesh, 0, 1024, 375, 3000);
+  rajmachawal = map(nirdesh,375,3000, 400, 50);
+  Serial.print("Theoretical RPM Currently: ");
+  Serial.println(rajmachawal);
+  darpan.setCursor(0,0);
+  darpan.print("  Adjust RPM:   ");
+  darpan.setCursor(6, 1);
+  darpan.print(rajmachawal);
+    if(debounce(set)==HIGH){
+  break;
+  }
+ }
+  darpan.setCursor(0,0);
+  darpan.print("Set RPM:  ");
+  darpan.setCursor(10, 0);
+  darpan.print(rajmachawal);
+  darpan.setCursor(0,1);
+  darpan.print("[RST] -to Change");
 }
 
 void loop() {
-deri = bakisabbakwas();
+ 
 digitalWrite(kadam, HIGH);
-delayMicroseconds(deri);
+delayMicroseconds(nirdesh);
 digitalWrite(kadam, LOW);
-delayMicroseconds(deri);
+delayMicroseconds(nirdesh);
 }
 
-int bakisabbakwas(){
-  nirdesh = analogRead(A0);
-  nirdesh = map(nirdesh, 0, 1024, 150, 15000);
-  rajmachawal = map(nirdesh, 150,15000, 1000, 10);
-  Serial.print("Theoretical RPM Currently: ");
-  Serial.print(rajmachawal);
-  darpan.setCursor(0,0);
-  darpan.write("Theoretical RPM:");
-  darpan.setCursor(6,1);
-  darpan.write(rajmachawal);
-  return nirdesh;
+bool debounce(int pin){
+bool a= digitalRead(pin);
+delay(50);
+bool b= digitalRead(pin);
+if(a!=b){
+debounce(pin);
 }
+return a;
+}
+  
+
+  
+  
+ 
